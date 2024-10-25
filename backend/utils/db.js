fa7e399b9f279d011d78a9cf3c2039720bee618a
M-sufyanshoukat <sufyanshoukat2122@gmail.com>
2024-10-25 20:17:42 +0500

    db_Updated

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+
+     const MONGO_URL = "mongodb://muhammadsufyan1520:12345@cluster0-shard-00-00.cxxfr.mongodb.net:27017,cluster0-shard-00-01.cxxfr.mongodb.net:27017,cluster0-shard-00-02.cxxfr.mongodb.net:27017/?ssl=true&replicaSet=atlas-yyokfr-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(MONGO_URL);
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
